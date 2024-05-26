20ef3f6cb180782454d4bfd567e2f858e54d5403
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-26 22:22:30 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,38 @@
+//1-misol
+//let n=+prompt("sonni kiriting")
+//
+//for(let i=1; i<=n;i--){
+//console .log(i);
+//} 2-misol
+//let n=+prompt("sonni kiriting")
+//
+//for(let i=1; i>=n;i--){
+//console .log(i);
+//}
+//3-misol
+//let n=+prompt("sonni kiriting");
+//for(let i=1;i<=n;i++);{
+//    console.log(i+n)
+//}
+//4-misol
+//let n=+prompt("sonni kiriting");
+//for(let i=1;i<=n;i%2=0);{
+//    console .log(i)
+//}
+//5-misol
+//let n=+prompt("sonni kiriting");
+//for(let i=1;i<=n;i%2=1);{
+//    console .log(i)
+//}
+//6-misol
+//let n=+prompt("sonni kiriting")
+//let k=+prompt("sonni kriting")
+//for(let i=1; k<=n;n++ ){
+//    console .log(k)
+//}
+//7-masala
+//let a=+prompt("sonni kiriting")
+//let b=+prompt("sonni kiriting")
+//for(  let i=1 a<=b; a++){
+//    console.log(a,b)
+//}
