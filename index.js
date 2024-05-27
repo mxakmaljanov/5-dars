a346eadb2bc7fd6f3df4fdb3a1c9d184707c0dde
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-27 09:24:30 +0500

    uy ishi

--- index.js
+++ index.js
@@ -36,3 +36,18 @@
 //for(  let i=1 a<=b; a++){
 //    console.log(a,b)
 //}
+//8-misol
+//let k=5
+//for(let i=+prompt("sonni kiting"); k<=i;k++){
+//    console.log(k)
+//}
+//9-misol
+//let k=7
+//for(let i=+prompt("sonni kiting"); k<=i;k/10){
+//    console.log(k)
+//}
+//0-misol
+//et k=6
+//or(let i=+prompt("sonni kiting"); k<=i;k+0.1){
+//   console.log(k)
+//
